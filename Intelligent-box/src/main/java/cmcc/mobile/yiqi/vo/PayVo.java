f1a05663ab281dc421f940ac21595070f923eaee
wubenjiang@hzhc-tech.com <wubenjiang@hzhc-tech.com>
2018-05-28 09:32:22 +0800

    修改下发指令逻辑

---
+++ Intelligent-box/src/main/java/cmcc/mobile/yiqi/vo/PayVo.java
@@ -0,0 +1,65 @@
+package cmcc.mobile.yiqi.vo;
+
+public class PayVo {
+
+	private String timestamp ;
+	private String nonceStr ;
+	private String appId ;
+	private String packageValue ;
+	private String paySign ;
+	private String orderNo ;
+	private String totalFee ;
+	private String signType ;
+	
+	
+	public String getSignType() {
+		return signType;
+	}
+	public void setSignType(String signType) {
+		this.signType = signType;
+	}
+	public String getTimestamp() {
+		return timestamp;
+	}
+	public void setTimestamp(String timestamp) {
+		this.timestamp = timestamp;
+	}
+	public String getNonceStr() {
+		return nonceStr;
+	}
+	public void setNonceStr(String nonceStr) {
+		this.nonceStr = nonceStr;
+	}
+	public String getAppId() {
+		return appId;
+	}
+	public void setAppId(String appId) {
+		this.appId = appId;
+	}
+	public String getPackageValue() {
+		return packageValue;
+	}
+	public void setPackageValue(String packageValue) {
+		this.packageValue = packageValue;
+	}
+	public String getPaySign() {
+		return paySign;
+	}
+	public void setPaySign(String paySign) {
+		this.paySign = paySign;
+	}
+	public String getOrderNo() {
+		return orderNo;
+	}
+	public void setOrderNo(String orderNo) {
+		this.orderNo = orderNo;
+	}
+	public String getTotalFee() {
+		return totalFee;
+	}
+	public void setTotalFee(String totalFee) {
+		this.totalFee = totalFee;
+	}
+	
+	
+}
